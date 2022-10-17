2af7816e1b74b6e6ba549dce55a3dcab9f4f1562
Brian <nieh_brian@yahoo.com>
2022-10-17 01:51:14 -0700

    Added addteam and deconstuctor

--- league.cpp
+++ league.cpp
@@ -1,38 +1,62 @@
 #include "league.h"
 
-/*DESCRIPTION*/
+/*	Go through all of the Teams in the teams array
+	and print each one using their print function*/
 void League::printTeams()
 {
-	//Go through all of the Teams in the teams array and print each one using their
-	//print function
-
-	//TODO
+	for (int i = 0; i < count; i++)
+	{
+		teams[i]->printTeam();
+	}
 }
 
-/**/
-void League::addTeam(Team* new_team)
+/*	This should add the passed new_team to the end of the list of Teams
+	Remember to set the size if you make the array bigger and to update the count
+	variable for each Team added.*/
+void League::addTeam(Team *new_team)
 {
-	//This should add the passed new_team to the end of the list of Teams
-	//Remember to set the size if you make the array bigger and to update the count
-	//variable for each Team added.
+	Team **tmp;
+	// If the array is unallocated, allocate it
+	if (teams == nullptr)
+	{
+		teams = new Team *[RESIZE_AMOUNT];
+		size = 4;
+	}
 
-	//TODO
+	// If the array is out of space, make a bigger array and deep copy over all of the old Team*s
+	if (count >= size)
+	{
+		tmp = new Team *[size + RESIZE_AMOUNT];
+		for (int i = 0; i < count; i++)
+		{
+			tmp[i] = teams[i];
+		}
+		delete teams;
+		teams = new Team *[size + RESIZE_AMOUNT];
+		teams = tmp;
+		tmp = nullptr;
+		size += 4;
+	}
 
-	//If the array is unallocated, allocate it
-	//If the array is out of space, make a bigger array and deep copy over all of the old Team*s
-
-	//Regardless of if a resize was needed, add the new_team to the next open space and update the count
+	// Regardless of if a resize was needed, add the new_team to the next open space and update the count
+	teams[count] = new_team;
+	count++;
 }
 
 /**/
 League::~League()
 {
-	//TODO
+	// TODO
 
-	//Start by deleting all of the teams in the array (similar to how the Team destructor deleted all its Players)
+	// Start by deleting all of the teams in the array (similar to how the Team destructor deleted all its Players)
+	for (int i = 0; i < count; i++)
+	{
+		delete teams[i];
+	}
+	delete[] teams;
 
-	//After the contents of the array are done being deallocated, delete the array that was storing all the Teams
-	//Delete the teams array the standard way you deallocate arrays (HINT: How does delete work on arrays?)
+	// After the contents of the array are done being deallocated, delete the array that was storing all the Teams
+	// Delete the teams array the standard way you deallocate arrays (HINT: How does delete work on arrays?)
 
-	//This should be about 3 lines of code
+	// This should be about 3 lines of code
 }
